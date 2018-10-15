c5f0c850387b7e568fffaa16008da843e230d0a2
vkhristenko <vdkhristenko1991@gmail.com>
2018-10-15 17:28:04 +0200

    testing sbatch submission

--- jube/cmssw_reco/setup.sh
+++ jube/cmssw_reco/setup.sh
@@ -9,8 +9,8 @@ export SCRAM_ARCH=slc7_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 # setup a release
-cd $WORKDI
+cd $WORKDIR
 scram project CMSSW $RELEASE
-cd $RELEASE/src
+cd $WORKDIR/$RELEASE/src
 eval `scramv1 runtime -sh`
 scram b -v -j 8
