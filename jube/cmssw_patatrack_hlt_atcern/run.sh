178b5d67473fec8ed67d84744111f25eb869b322
vkhristenko <vdkhristenko1991@gmail.com>
2020-09-22 13:57:00 +0200

    add cfgs for cenr

---
+++ jube/cmssw_patatrack_hlt_atcern/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 
+WORKDIR=$1
+NUM_CORES=$2
+RELEASE=CMSSW_11_1_0_Patatrack
+CFG=/p/project/cdeep/khristenko1/cmsrun_configs/hcal/gpu.py
+export SCRAM_ARCH=slc7_amd64_gcc820
+
+source /p/project/cdeep/khristenko1/cms_env.sh
+
+# we will work in some tmp dir
+[ ! -d "$WORKDIR" ] && echo "$WORKDIR folder does not exist!" && exit 1
+cd $WORKDIR/$RELEASE/src
+
+# init 
+eval `scramv1 runtime -sh`
+
+# run
+echo "running in $WORKDIR with $NUM_CORES cores per process"
+LD_PRELOAD=/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/cuda/11.0.1/drivers/libcuda.so.450.36.06:/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/cuda/11.0.1/drivers/libnvidia-ptxjitcompiler.so.450.36.06 /p/project/cdeep/khristenko1/patatrack-scripts/benchmark $CFG $NUM_CORES > results.log 2>&1
+
+# parse the logs to get average throughput
+input="results.log"
+counter=0
+while IFS= read -r line
+do
+    #echo "$counter --> $line"
+    if [ $counter -eq 15 ]
+    then
+        echo "average throughput = $line" # __ THIS GUY PRINTS AN AVERAGE THROUGHPUT OF 4 RUNS __
+    fi
+    counter=$((counter+1))
+done < "$input"
+
+# print the rest of the log
+echo ">>> full log"
+cat $input
+echo "<<< full log"
---
+++ jube/cmssw_patatrack_hlt_atcern/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "PYTHONPATH = $PYTHONPATH"
+
+# 
+WORKDIR=$1
+NUM_CORES=$2
+#RELEASE=CMSSW_11_1_0_pre8_Patatrack
+RELEASE=CMSSW_11_1_0_Patatrack
+export SCRAM_ARCH=slc7_amd64_gcc820
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+# we will work in some tmp dir
+[ -d "$WORKDIR" ] && rm -rf $WORKDIR
+mkdir $WORKDIR
+cd $WORKDIR
+
+# check out a release
+scram project CMSSW $RELEASE
+cd $WORKDIR/$RELEASE/src
+eval `scramv1 runtime -sh`
+git cms-init -x cms-patatrack
+
+# bring the current release branch up to the head
+git checkout CMSSW_11_1_X_Patatrack
+git diff $CMSSW_VERSION --name-only --no-renames | cut -d/ -f-2 | sort -u | xargs -r git cms-addpkg
+git cms-checkdeps -a
+
+echo "PYTHONPATH = $PYTHONPATH"
+
+# build
+scram b -v -j $NUM_CORES
+
+echo "PYTHONPATH = $PYTHONPATH"
